5ecd3bbff8130421d49522c56b504fc877aa3a85
ThomasDouglass0172 <100816798+ThomasDouglass0172@users.noreply.github.com>
2022-05-12 12:25:57 +0930

    sort

---
+++ Sort.cpp
@@ -0,0 +1,7 @@
+#include "Sort.h"
+
+Using namespace std; 
+
+vector<int> sort(std::vector<int> list){
+
+}
---
+++ Sort.h
@@ -0,0 +1,12 @@
+#ifndef SORT_H
+#define SORT_H
+#include <iostream>
+
+class Sort{
+
+public:
+std::vector<int> sort(std::vector<int> list); 
+private: 
+
+};
+#endif
